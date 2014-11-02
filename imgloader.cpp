3b2dc45d03e838dd3dc43a33ed66222eeb004455
alexthered <hiep.dv179@gmail.com>
2014-11-02 01:24:37 +0100

    Image Loader class: add function for loading an image or a directory, show and clear input data

--- imgloader.cpp
+++ imgloader.cpp
@@ -1,8 +1,82 @@
 #include "imgloader.h"
+#include "opencv2/highgui/highgui.hpp"
+#include <QDirIterator>
+#include <QString>
 
-//use Boost library to read all images in a folder
-#include <boost/filesystem.hpp>
+using namespace std;
+using namespace cv;
 
+
+//ctor
 ImgLoader::ImgLoader()
 {
 }
+
+int ImgLoader::AddInputFromDir(const string path_to_dir)
+{
+    cout << "[Image Loader] Read images from " << path_to_dir << endl;
+    //convert the directory path to QString
+    QString q_path_to_dir = QString::fromStdString(path_to_dir);
+
+    //iterate over the input directory to load all images
+    QDirIterator dir_iter(QString(q_path_to_dir),
+                          QDir::AllDirs|QDir::Files|QDir::NoSymLinks,
+                          QDirIterator::Subdirectories
+                          );
+    while(dir_iter.hasNext())
+    {
+        dir_iter.next();
+
+        //make sure it collects only image files
+        if (dir_iter.fileInfo().completeSuffix().contains("jpg", Qt::CaseInsensitive) ||
+            dir_iter.fileInfo().completeSuffix().contains("jpeg", Qt::CaseInsensitive) ||
+            dir_iter.fileInfo().completeSuffix().contains("png", Qt::CaseInsensitive)
+                ){
+            // assign current file name to current_file
+            QString current_file = dir_iter.fileInfo().absoluteFilePath();
+
+            //add image file whose name is current_file
+            if(!AddInput(current_file.toStdString()))
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int ImgLoader::AddInput(const string path_to_file)
+{
+    cout << "[Image Loader] Read image at " << path_to_file << endl;
+    //open image
+    Mat cur_img = cv::imread(path_to_file,1);
+    if (!cur_img.data){
+        cerr << "[Image Loader] Cannot load input" << path_to_file << endl;
+        return 0;
+    }
+
+    //push the loaded image and increment the num_input
+    img_input.push_back(cur_img.clone());
+
+    return 1;
+}
+
+//clear all the data
+void ImgLoader::ClearInput()
+{
+    cout << "[Image Loader] Clear data!" << endl;
+    img_input.clear();
+}
+
+//show all loaded data
+void ImgLoader::ShowInputImg()
+{
+    cout << "[Image Loader] Showing image....";
+    vector<Mat>::iterator iter = img_input.begin();
+    while(iter!=img_input.end()){
+        imshow("Input", (*iter));
+        waitKey(0);
+        iter++;
+    }
+}
+
+
+
--- imgloader.h
+++ imgloader.h
@@ -18,17 +18,20 @@ class ImgLoader
 public:
     ImgLoader();
 
-    //load input from a folder
-    int LoadInputFromDir(std::string path_to_dir);
-    //add additional input from a folder
-    int AddInputFromDir(std::string path_to_dir);
-    //add additional image
-    int AddInput(std::string path_to_file);
-    int& num_input(){return num_input;}
+    //Add input from a folder
+    int AddInputFromDir(const std::string path_to_dir);
+    //add a single image
+    int AddInput(const std::string path_to_file);
+    //clear the loaded data
+    void ClearInput();
+    //show the loaded data
+    void ShowInputImg();
+
+    cv::vector<cv::Mat>& get_input_img(){ return img_input; }
+    int get_num_input() const { return img_input.size(); }
 
 private:
     std::vector<cv::Mat> img_input;
-    int num_input;
 };
 
 #endif // IMGLOADER_H
