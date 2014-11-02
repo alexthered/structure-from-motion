3b4fed9a35b96f822412f56cbbb795fe1bef2093
alexthered <hiep.dv179@gmail.com>
2014-11-02 11:42:43 +0100

    fix unintialized reference error

--- featurematcher.cpp
+++ featurematcher.cpp
@@ -1,6 +1,6 @@
 #include "featurematcher.h"
 
-FeatureMatcher::FeatureMatcher(bool _use_gpu):
-    use_gpu(_use_gpu)
+FeatureMatcher::FeatureMatcher(bool _use_gpu, std::vector<cv::Mat>& _img_input):
+    use_gpu(_use_gpu), img_input(_img_input)
 {
 }
--- featurematcher.h
+++ featurematcher.h
@@ -13,12 +13,17 @@
 class FeatureMatcher
 {
 public:
-    FeatureMatcher(bool _use_gpu = false);
+    FeatureMatcher(bool _use_gpu, std::vector<cv::Mat>& _img_input);
 
     virtual void MatchFeatures(int idx_i, int idx_j, std::vector<cv::DMatch>* matches)= 0;
 
 protected:
     bool use_gpu;
+    //set of images to find matching features
+    std::vector<cv::Mat>& img_input;
+    //set of keypoint to match
+    std::vector<std::vector <cv::KeyPoint> > img_kpts;
+
 };
 
 #endif // FEATUREMATCHER_H
