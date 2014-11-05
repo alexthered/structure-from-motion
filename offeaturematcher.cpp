8a8fdde21d662a10a38b11047a9c9d60789ab84a
alexthered <hiep.dv179@gmail.com>
2014-11-06 00:07:00 +0100

    Collect features on each image using FastFeatureDetector

--- offeaturematcher.cpp
+++ offeaturematcher.cpp
@@ -12,9 +12,13 @@ OFFeatureMatcher::OFFeatureMatcher(bool _use_gpu, vector<cv::Mat>& _img_input)
 
 void OFFeatureMatcher::CollectFeatures()
 {
+    cv::FastFeatureDetector ffd;
 
+    //collect features on each image
+    ffd.detect(img_input, img_kpts);
 }
 
+//Perform feature matching between image i and j using optical flow
 void OFFeatureMatcher::MatchFeatures(int idx_i, int idx_j, std::vector<cv::DMatch> *matches)
 {
 
