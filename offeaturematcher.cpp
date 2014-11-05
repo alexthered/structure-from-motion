c1955f0c596fe52065bd034b53b7b0ca5bc7cada
Doan Vu Hiep <doanvuhiep@ubuntu>
2014-11-05 23:46:47 +0100

    Add feature matching class using optical flow

---
+++ offeaturematcher.cpp
@@ -0,0 +1,5 @@
+#include "offeaturematcher.h"
+
+offeaturematcher::offeaturematcher()
+{
+}
---
+++ offeaturematcher.h
@@ -0,0 +1,10 @@
+#ifndef OFFEATUREMATCHER_H
+#define OFFEATUREMATCHER_H
+
+class offeaturematcher : public featurematcher
+{
+public:
+    offeaturematcher();
+};
+
+#endif // OFFEATUREMATCHER_H
