7f67dac719db06bee3d345c92e1def57695d5295
alexthered <hiep.dv179@gmail.com>
2014-11-06 00:03:27 +0100

    Add interface for OFFeatureMatcher class

--- offeaturematcher.cpp
+++ offeaturematcher.cpp
@@ -1,5 +1,21 @@
 #include "offeaturematcher.h"
+#include <set>
+#include <opencv2/highgui/highgui.hpp>
 
-offeaturematcher::offeaturematcher()
+using namespace std;
+
+//c'tor
+OFFeatureMatcher::OFFeatureMatcher(bool _use_gpu, vector<cv::Mat>& _img_input)
+    : FeatureMatcher(_use_gpu, _img_input)
 {
+}
+
+void OFFeatureMatcher::CollectFeatures()
+{
+
+}
+
+void OFFeatureMatcher::MatchFeatures(int idx_i, int idx_j, std::vector<cv::DMatch> *matches)
+{
+
 }
--- offeaturematcher.h
+++ offeaturematcher.h
@@ -1,10 +1,20 @@
 #ifndef OFFEATUREMATCHER_H
 #define OFFEATUREMATCHER_H
 
-class offeaturematcher : public featurematcher
+#include "featurematcher.h"
+#include <iostream>
+#include <opencv2/video/video.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/flann/flann.hpp>
+
+class OFFeatureMatcher : public FeatureMatcher
 {
 public:
-    offeaturematcher();
+    OFFeatureMatcher(bool _use_gpu , std::vector<cv::Mat>& _img_input);
+    void MatchFeatures(int idx_i, int idx_j, std::vector<cv::DMatch> *matches );
+
+private:
+    void CollectFeatures();
 };
 
 #endif // OFFEATUREMATCHER_H
